110b5023bf27fc87e8025db05bb5696e4e83bf4b
Osigelialex <osigelialex@gmail.com>
2023-07-13 20:26:27 +0100

    Added task 6

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,22 @@
+-- script that creates a stored procedure AddBonus that adds a new correction for a student
+DELIMITER $$
+
+CREATE PROCEDURE AddBonus (
+  IN user_id INT,
+  IN project_name VARCHAR(255),
+  IN score INT
+)
+
+BEGIN
+  DECLARE project_id INT,
+  SELECT INTO project_id FROM projects WHERE name = project_name;
+
+  IF project_id IS NULL 
+  THEN
+    INSERT INTO projects (name) VALUE (project_name);
+  ELSE
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+  END IF;
+END $$
+
+DELIMITER ;
