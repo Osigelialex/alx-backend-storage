0d7f17c15b502f2a0eac05056f3d7bc93087d511
Osigelialex <osigelialex@gmail.com>
2023-07-13 23:07:53 +0100

    task 11

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -3,4 +3,4 @@
 CREATE VIEW need_meeting
 AS
 SELECT * FROM students WHERE score < 80 AND 
-(last_meeting = NULL OR MONTH(last_meeting) = MONTH(NOW() - INTERVAL 1 MONTH));
+(last_meeting = NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
