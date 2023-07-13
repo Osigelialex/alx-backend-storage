642cfdd961108bf9fca3bfc4460dc4ad72b5d9e6
Alex Osigeli <97721950+Osigelialex@users.noreply.github.com>
2023-07-13 09:40:40 +0100

    Update 3-glam_rock.sql
    Changed column name to lifespan

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, COALESCE(split, 2022) - formed AS longetivity
+SELECT band_name, COALESCE(split, 2022) - formed AS lifespan
 FROM metal_bands
 WHERE style like '%Glam rock%'
-ORDER BY longetivity DESC;
+ORDER BY lifespan DESC;
