70a6abae58cd98f98ac265cce826596deaf2bf8a
Osigelialex <osigelialex@gmail.com>
2023-07-15 21:26:55 +0100

    added advanced task

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,20 @@
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
+BEGIN
+    DECLARE sum_weights FLOAT;
+    DECLARE weighted_sum FLOAT;
+
+    SELECT SUM(weight) INTO sum_weights FROM projects;
+
+    SELECT SUM(corrections.score * projects.weight) INTO weighted_sum
+    FROM corrections
+    JOIN projects ON corrections.project_id = projects.id
+    WHERE corrections.user_id = user_id;
+
+    UPDATE users SET average_score = weighted_sum / sum_weights WHERE id = user_id;
+END $$
+
+DELIMITER ;
