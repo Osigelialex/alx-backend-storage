f0590a6b6369a5b18d379b42d6f2b7d5c9850f51
Osigelialex <osigelialex@gmail.com>
2023-07-17 23:04:09 +0100

    Task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+A module to list all documents from collection
+"""
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+  """lists all documents in collection
+
+  Args:
+      mongo_collection (collection object):
+        collection object to get documents from
+  """
+  return MongoClient().mongo_collection.find()
