18ff3b0b0994809b0c5fb00330660b39325a4ce8
Osigelialex <osigelialex@gmail.com>
2023-07-18 11:04:21 +0100

    Update 8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,17 +2,17 @@
 """
 A module to list all documents from collection
 """
-def list_all(mongo_collection):
-  """lists all documents in collection
+from pymongo import MongoClient
 
-  Args:
+
+def list_all(mongo_collection):
+    """lists all documents in collection
+
+    Args:
       mongo_collection (collection object):
         collection object to get documents from
-  """
-  client = MongoClient()
-  db = client['my_db']
-  collection = db.mongo_collection
-  return collection.find()
-
-if __name__ == "__main__":
-  from pymongo import MongoClient
+    """
+    client = MongoClient()
+    db = client['my_db']
+    collection = db.mongo_collection
+    return collection.find()
