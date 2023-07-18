60fe2ca5ce58572a1a4625b012b7b072444be7d2
Osigelialex <osigelialex@gmail.com>
2023-07-18 11:17:14 +0100

    add task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" 
+A module that inserts a new document in a collection based on kwargs
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+  """inserts a new document in a collection based on kwargs
+
+  Args:
+      mongo_collection (object): mongo collection object
+  """
+  mongo_collection.insert_one(kwargs)
+  return mongo_collection['_id']
