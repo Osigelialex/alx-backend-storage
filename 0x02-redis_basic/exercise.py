17cb2d44ea8995d5cf0efe26567fe4e6525512de
Osigelialex <osigelialex@gmail.com>
2023-07-19 22:00:55 +0100

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ A module that writes string to redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional, Any
 
 
 class Cache:
@@ -23,3 +23,16 @@ class Cache:
         key = uuid.uuid4()
         self._redis.set(str(key), (data))
         return str(key)
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> \
+            Union[Any,  bytes, None]:
+        """returns original type of key"""
+        return fn(self._redis.get(key)) if fn else self._redis.get(key)
+
+    def get_int(self, key: bytes) -> int:
+        """converts key to int type"""
+        return int.from_bytes(key, "big")
+
+    def get_str(self, key: bytes) -> str:
+        """converts key to str type"""
+        return str(key, 'UTF-8')
