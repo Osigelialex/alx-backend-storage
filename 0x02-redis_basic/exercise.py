5c7cac68ac85a272603b037496a99440466d4cfc
Osigelialex <osigelialex@gmail.com>
2023-07-19 19:51:08 +0100

    converted uuid to string

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,16 +15,17 @@ class Cache:
     """
     def __init__(self):
         """initializes the Cache"""
-        _redis = redis.Redis().flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
-    def store(data) -> str:
+    def store(self, data) -> str:
         """
         stores data in redis using random key
 
         Args:
             data(string) - value to store in redis
         """
-        key = uuid.uuid4()
+        key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
