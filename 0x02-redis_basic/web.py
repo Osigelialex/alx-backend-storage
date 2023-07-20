09126772942cd797965e425feb13a36099e8cd29
Osigelialex <osigelialex@gmail.com>
2023-07-20 23:21:44 +0100

    Fixed mypy issue

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,27 +4,23 @@ A module that Implements an expiring web cache and tracker
 """
 import requests
 import redis
+from typing import Union
 
 
-def cache_response(method):
-    """stores response from html page in redis cache"""
-    def get_page(url):
-        r = redis.Redis()
-        response = method(url)
-        key = "count:" + url
-
-        if not r.get(key):
-            r.set(key, 0)
-            r.set('content', response)
-            r.expire('content', 10)
-        else:
-            r.incr(key)
-        return r.get('content')
-    return get_page
-
-
-@cache_response
-def get_page(url: str):
+def get_page(url: str) -> Union[bytes, None]:
     """obtains html content from url"""
+    r = redis.Redis()
     response = requests.get(url).content
+    key = "count:" + url
+
+    if not r.get(key):
+        r.set(key, 0)
+        r.set('content', response)
+        r.expire('content', 10)
+    else:
+        r.incr(key)
+
+    if r.get('content'):
+        return r.get('content')
+
     return response
