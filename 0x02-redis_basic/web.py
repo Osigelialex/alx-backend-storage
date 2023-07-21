39faac7429c56d630834379d46a64145bc8ba60a
Osigelialex <osigelialex@gmail.com>
2023-07-21 09:57:08 +0100

    update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,27 +1,39 @@
 #!/usr/bin/python3
 """
-A module that Implements an expiring web cache and tracker
+A module for implementing a web cache and tracker
 """
-import requests
 import redis
-from typing import Union
+import requests
+from requests.exceptions import InvalidURL, RequestException
 
 
-def get_page(url: str) -> Union[str, None]:
-    """obtains html content from url"""
+def get_page(url: str) -> str:
+    """
+    Obtains html from a page and displays it
+    """
     redis_client = redis.Redis()
-    response = requests.get(url)
-    key = "count:" + url
+    key = 'count:' + url
+    CACHE_EXPIRY = 10
 
-    if response.status_code == 200:
-        redis_client.setex('content', 10, response.text)
-
+    # increment tracker count for url
     if not redis_client.get(key):
-        redis_client.set(key, 10)
+        redis_client.set(key, 0)
     else:
         redis_client.incr(key)
 
-    if redis_client.get('content'):
-        return redis_client.get('content')
+    # check for cached content
+    if redis_client.get(url):
+        return redis_client.get(url)
 
-    return response.text
+    # cache response with expiry
+    try:
+        response = requests.get(url)
+        html_content = response.text
+        redis_client.setex(url, CACHE_EXPIRY, html_content)
+        return html_content
+    except InvalidURL as e:
+        print('Invalid URL', e)
+    except RequestException as e:
+        print('Error', e)
+
+    return ''
