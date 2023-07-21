c991b37111a6532e7cc9f7ac6bfd583a35be7bed
Osigelialex <osigelialex@gmail.com>
2023-07-21 10:19:46 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -16,21 +16,22 @@ def get_page(url: str) -> str:
     CACHE_EXPIRY = 10
 
     # increment tracker count for url
-    if not redis_client.get(key):
+    if not redis_client.exists(key):
         redis_client.set(key, 0)
     else:
         redis_client.incr(key)
 
     # check for cached content
-    if redis_client.get(url):
-        return redis_client.get(url)
+    if redis_client.exists(url):
+        cached_content = redis_client.get(url).decode('utf-8')
+        return cached_content
 
     # cache response with expiry
     try:
         response = requests.get(url)
         html_content = response.text
-        redis_client.setex(url, CACHE_EXPIRY, html_content)
-        return html_content
+        redis_client.setex(url, CACHE_EXPIRY, html_content.encode('utf-8'))
+        return str(html_content)
     except InvalidURL as e:
         print('Invalid URL', e)
     except RequestException as e:
